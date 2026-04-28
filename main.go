737e33db97d64b8b947f5e11d3b9d02f2139e2f2
tetr1chaan <tetri385@gmail.com>
2026-04-28 20:50:07 +0500

    The base

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ type Task struct {
 	Deadline string
 }
 
+func (a *Task) NewTask(task string, deadline string) {
+	a.WhTask = task
+	a.Deadline = deadline
+}
 func main() {
 	flag := true
-	for flag != false {
+	var AllTasks []Task
+	for flag {
 		var s string
 		fmt.Println("Do you have new task answer yes/no:")
 		fmt.Scanln(&s)
@@ -25,14 +30,34 @@ func main() {
 				fmt.Println("Error of reading:", err)
 				return
 			}
-			NewTask := input
+			Newt := input
 			fmt.Println("Enter deadline of your new task:")
 			input1, err := reader.ReadString('\n')
 			if err != nil {
 				fmt.Println("Error of reading:", err)
 				return
 			}
-			NewDeadline := input1
+			NewD := input1
+			var tusk Task
+			tusk.NewTask(Newt, NewD)
+			AllTasks = append(AllTasks, tusk)
+		} else {
+			flag = false
 		}
+	}
+	fmt.Println("Do you want check all tasks?")
+	var checker string
+	fmt.Scanln(&checker)
+	if checker == "Yes" || checker == "yes" || checker == "YES" {
+		i := 0
+		for _, task := range AllTasks {
+			i++
+			fmt.Print("Task №", i, "\n")
+			fmt.Print(task.WhTask)
+			fmt.Print(task.Deadline)
+			fmt.Println("---------------------------------")
+		}
+	} else {
+		return
 	}
 }
